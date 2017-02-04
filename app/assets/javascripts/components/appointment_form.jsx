c6133a0784408c03edbc3a265773f0933f514845
Jaroslav Horak <jaroslavhorak@gmail.com>
2017-02-04 10:53:01 +0100

    Section 3.6

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -11,15 +11,31 @@ var AppointmentForm = React.createClass({
     this.props.onFormSubmit();
   },
 
+  setApptTime: function(e) {
+    var name = 'appt_time';
+    var obj = {};
+    if(obj[name] = e.toDate()) {
+      this.props.onUserInput(obj);
+    }
+  },
+
   render: function() {
+    var inputProps = {
+      name: 'appt_time'
+    };
+
     return (
       <div>
         <h2>Make a new appointment</h2>
         <form onSubmit={this.handleSubmit}>
           <input value={this.props.title} onChange={this.handleChange}
             name="title" placeholder="Appointment Title" type="text"/>
-          <input value={this.props.appt_time} onChange={this.handleChange}
-            name="appt_time" placeholder="Date and Time" type="text"/>
+          <Datetime inputProps={inputProps}
+            value={this.props.appt_time}
+            open={true}
+            input= {false}
+            onChange={this.setApptTime}
+          />
           <input type="submit" value="Make Appointment" />
         </form>
       </div>
